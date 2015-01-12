be8ad5516aad44ccb72d953aed9e4eda42244124
Sung Choi <m.sung.choi@gmail.com>
2015-01-11 19:48:49 -0500

    Adds Like model with one attribute, chirp, of the type references via rails scaffolding. Steps:     rails generate scaffold Like chirp:references     git add .     git commit -m "Adds Like model with one attribute, chirp, of the type references via rails scaffolding."     git push origin master

---
+++ app/views/likes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @like, :id, :chirp_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :likes
+
   resources :chirps
 
   # The priority is based upon order of creation: first created -> highest priority.
